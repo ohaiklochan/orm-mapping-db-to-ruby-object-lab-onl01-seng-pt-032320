304596f1c86c173ffc84aeb8a0a8185345e50809
Chloe Steffens <caforonda103@yahoo.com>
2020-06-08 02:54:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,9 @@ class Student
   end
 
   def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
     DB[:conn].execute(sql)
   end
 
